e7b7259739737e20d34e8bb997962ff076a3fc70
Jairo Calcina <jairo.calcina@outlook.com>
2024-04-14 11:34:22 -0400

    fix: fix AccountController error messages

--- Src/Controllers/AccountController.cs
+++ Src/Controllers/AccountController.cs
@@ -47,14 +47,14 @@ public class AccountController : BaseApiController
 
         if (accountDto == null)
         {
-            return TypedResults.BadRequest("Invalid credentials");
+            return TypedResults.BadRequest("Credentials are invalid");
         }
 
         PasswordDto? passwordDto = await _accountRepository.GetPasswordAsync(loginDto.Email);
 
         if (passwordDto == null)
         {
-            return TypedResults.BadRequest("Invalid credentials");
+            return TypedResults.BadRequest("Internal server error, please try again later");
         }
 
         using var hmac = new HMACSHA512(passwordDto.PasswordSalt);
@@ -62,7 +62,7 @@ public class AccountController : BaseApiController
 
         if (!computedHashPassword.SequenceEqual(passwordDto.PasswordHash))
         {
-            return TypedResults.BadRequest("Invalid credentials");
+            return TypedResults.BadRequest("Credentials are invalid");
         }
 
         return TypedResults.Ok(accountDto);
